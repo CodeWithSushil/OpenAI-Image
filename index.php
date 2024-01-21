83a92159cf8206b709388e13d237157d4d6122cd
CodeWithSushil <sushilkumbhar77@gmail.com>
2024-01-21 21:51:16 +0530

    Dev v0.0.02

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
-spl_autoload_register(function($class){
-  echo "hello php!";
+define("APP_ROOT", __DIR__);
+
+require_once APP_ROOT . "/vendor/autoload.php";
+
+// autoloader for namespaced classes
+spl_autoload_register(function ($class)
+{
+  $classFile = str_replace("\\", DIRECTORY_SEPARATOR, $class . ".php");
+  $classPath = APP_ROOT . "/app/" . $classFile;
+  
+  if (file_exists($classPath))
+  {
+    require_once $classPath;
+  }
 });
+
+session_start();
+
+use App\Services\Routes;
+
+$route = new Routes();
+require_once APP_ROOT ."/routes/Route.php";
+
+$route->handle();
---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Text-To-Image: OpenAI</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">-->
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+  <h1>home page</h1>
+  </body>
+</html>
