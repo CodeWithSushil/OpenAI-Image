031c3874d4fdeb9cc95e22ae2043eb39823451a7
CodeWithSushil <sushilkumbhar77@gmail.com>
2024-04-03 22:42:03 +0530

    use Bulma v1.0.0

--- public/pages/include/header.php
+++ public/pages/include/header.php
@@ -9,8 +9,8 @@ echo $trim = ucwords($name);
 ?> | Text-To-Image: OpenAI</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-<link href="public/assets/css/bulma.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
   </head>
   <body class="is-family-sans-serif">
 
