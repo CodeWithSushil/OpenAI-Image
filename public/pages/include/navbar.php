2d167c08b6e66caf3d6c8c9632430004f73f4466
CodeWithSushil <sushilkumbhar77@gmail.com>
2024-04-14 23:11:29 +0530

    update

---
+++ public/pages/include/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar is-link">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="/">
+      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F021%2F059%2F825%2Foriginal%2Fchatgpt-logo-chat-gpt-icon-on-green-background-free-vector.jpg&f=1&nofb=1&ipt=adcb2501e8d0efa258f610c7539b32f05106f16ee0d960d8f0ead21cbc6c854b&ipo=images" alt="Open-AI" width="28" height="28">
+    </a>
+    <div class="navbar-burger" data-target="navbar">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+  <div id="navbar" class="navbar-menu">
+    <div class="navbar-end">
+      <a class="navbar-item" href="/">Home</a>
+      <a class="navbar-item" href="#">About</a>
+      <a class="navbar-item" href="#">Services</a>
+      <a class="navbar-item" href="/login">Login</a>
+    </div>
+  </div>
+</nav>
