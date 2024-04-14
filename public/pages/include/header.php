e0355577c73c801b07939b2a50761fb6d39ed7e9
CodeWithSushil <sushilkumbhar77@gmail.com>
2024-04-14 20:02:59 +0530

    update

--- public/pages/include/header.php
+++ public/pages/include/header.php
@@ -9,14 +9,9 @@ echo $trim = ucwords($name);
 ?> | Text-To-Image: OpenAI</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-=======
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@latest/css/bulma.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
->>>>>>> dev
   </head>
   <body class="is-family-sans-serif">
 
