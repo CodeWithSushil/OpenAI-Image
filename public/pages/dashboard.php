8b5cb6d6f999bd38c327fa2c60586af362318857
CodeWithSushil <sushilkumbhar77@gmail.com>
2024-04-21 07:22:46 +0530

    file update

--- public/pages/dashboard.php
+++ public/pages/dashboard.php
@@ -1,3 +1,10 @@
-<h1>
-Dashboard Page
-</h1>
+<?php
+pageAdd("include/header.php");
+pageAdd("include/navbar.php");
+?>
+<select>
+<option>
+04/21/2024
+</option>
+</select>
+<?php pageAdd("include/footer.php"); ?>
