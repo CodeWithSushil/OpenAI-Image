f0753fc01c54050aeb19bbdfddf0a63168cbb144
CodeWithSushil <sushilkumbhar77@gmail.com>
2024-06-07 14:04:48 +0530

    packages update

--- public/index.php
+++ public/index.php
@@ -3,4 +3,12 @@
 
 <?php echo $_SESSION["username"]; ?>
 
+<form method="POST">
+<textarea>
+
+</textarea>
+<button class="is-link" type="submit">Send</button>
+
+</form>
+
 <?php pageAdd("include/footer.php"); ?>
