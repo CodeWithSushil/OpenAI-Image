715d45391591558479232144ec0b194d1d67ba4f
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-08-06 18:11:08 +0530

    css update

--- public/pages/include/header.php
+++ public/pages/include/header.php
@@ -11,7 +11,7 @@ echo $trim = ucwords($name);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="icon" type="image/png" href="public/assets/images/favicon.ico"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 <!--<link rel="stylesheet" href="public/assets/css/bulma.min.css">-->
   </head>
